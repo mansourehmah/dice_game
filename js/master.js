efcd9c5c9df0a3d51946b3ad3f8489f73e366ac9
mansoureh <mansoureh.1998@gmail.com>
2020-09-19 18:33:09 +0430

    add second page

--- js/master.js
+++ js/master.js
@@ -1,3 +1,7 @@
+var which_player = 0,
+  score = [0, 0],
+  noawards = true;
+
 function start() {
   var name = [];
   name[0] = document.getElementById("first").value;
@@ -9,5 +13,20 @@ function start() {
     name[1] = "user2";
   }
   document.getElementsByClassName("start")[0].style.top = "-100%";
-  console.log(name)
+
+  //start game
+  document.getElementsByClassName("play")[0].style.top = "0";
+  document.querySelector("#first-player>h2").innerHTML = name[0];
+  document.querySelector("#secound-player>h2").innerHTML = name[1];
+  _play();
+}
+
+function _play() {
+  if (which_player % 2 == 0) {
+    document.getElementById("first-player-deactive").style.top = "-100%";
+    document.getElementById("secound-player-deactive").style.top = "0";
+  } else {
+    document.getElementById("first-player-deactive").style.top = "0";
+    document.getElementById("secound-player-deactive").style.top = "-100%";
+  }
 }
