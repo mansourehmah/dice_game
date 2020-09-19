3b3a6d739d1bc8f4d6fc31b04bb9c7c8a10b4f86
mansoureh <mansoureh.1998@gmail.com>
2020-09-19 19:26:40 +0430

    set score

--- js/master.js
+++ js/master.js
@@ -1,6 +1,7 @@
 var which_player = 0,
   score = [0, 0],
   noawards = true;
+endgame = false;
 
 function start() {
   var name = [];
@@ -22,11 +23,125 @@ function start() {
 }
 
 function _play() {
-  if (which_player % 2 == 0) {
-    document.getElementById("first-player-deactive").style.top = "-100%";
-    document.getElementById("secound-player-deactive").style.top = "0";
+  if (!endgame) {
+    if (which_player % 2 == 0) {
+      document.getElementById("first-player-deactive").style.top = "-100%";
+      document.getElementById("secound-player-deactive").style.top = "0";
+    } else {
+      document.getElementById("first-player-deactive").style.top = "0";
+      document.getElementById("secound-player-deactive").style.top = "-100%";
+    }
+  }
+}
+// render random number
+function _rand() {
+  var random_number;
+  _play();
+  if (noawards) {
+    random_number = Math.floor(Math.random() * 6 + 1);
+    noawards = false;
   } else {
-    document.getElementById("first-player-deactive").style.top = "0";
-    document.getElementById("secound-player-deactive").style.top = "-100%";
+    noawards = true;
+    random_number = Math.floor(Math.random() * 5 + 1);
+  }
+  document.getElementById("rubic-one").setAttribute("class", "parent-rubic");
+  document.getElementById("rubic-two").setAttribute("class", "parent-rubic");
+  if (random_number == 1) {
+    if (which_player % 2 == 0) {
+      document
+        .getElementById("rubic-one")
+        .setAttribute("class", "parent-rubic anime-one");
+    } else {
+      document
+        .getElementById("rubic-two")
+        .setAttribute("class", "parent-rubic anime-one");
+    }
+  }
+  if (random_number == 2) {
+    if (which_player % 2 == 0) {
+      document
+        .getElementById("rubic-one")
+        .setAttribute("class", "parent-rubic anime-two");
+    } else {
+      document
+        .getElementById("rubic-two")
+        .setAttribute("class", "parent-rubic anime-two");
+    }
+  }
+  if (random_number == 3) {
+    if (which_player % 2 == 0) {
+      document
+        .getElementById("rubic-one")
+        .setAttribute("class", "parent-rubic anime-three");
+    } else {
+      document
+        .getElementById("rubic-two")
+        .setAttribute("class", "parent-rubic anime-three");
+    }
+  }
+  if (random_number == 4) {
+    if (which_player % 2 == 0) {
+      document
+        .getElementById("rubic-one")
+        .setAttribute("class", "parent-rubic anime-four");
+    } else {
+      document
+        .getElementById("rubic-two")
+        .setAttribute("class", "parent-rubic anime-four");
+    }
+  }
+  if (random_number == 5) {
+    if (which_player % 2 == 0) {
+      document
+        .getElementById("rubic-one")
+        .setAttribute("class", "parent-rubic anime-five");
+    } else {
+      document
+        .getElementById("rubic-two")
+        .setAttribute("class", "parent-rubic anime-five");
+    }
+  }
+  if (random_number == 6) {
+    if (which_player % 2 == 0) {
+      document
+        .getElementById("rubic-one")
+        .setAttribute("class", "parent-rubic anime-six");
+    } else {
+      document
+        .getElementById("rubic-two")
+        .setAttribute("class", "parent-rubic anime-six");
+    }
+  }
+
+  function set_score() {
+    if (which_player % 2 == 0) {
+      score[0] = score[0] + random_number;
+      document.getElementsByClassName("score-one")[0].innerHTML = score[0];
+    } else {
+      score[1] = score[1] + random_number;
+      document.getElementsByClassName("score-two")[0].innerHTML = score[1];
+    }
+    if (score[which_player % 2] >= 10) {
+      endgame = true;
+      if (which_player % 2 == 0) {
+        document.getElementById("secound-player-deactive").style.top = "0";
+        document.getElementById("winner-one").setAttribute("class", "active");
+      } else {
+        document.getElementById("first-player-deactive").style.top = "0";
+        document.getElementById("winner-two").setAttribute("class", "active");
+      }
+    }
+    which_player++;
+  }
+  setTimeout(set_score, 2000);
+  setTimeout(_reapet, 2200);
+  function _reapet() {
+    if (score[(which_player - 1) % 2] < 30) {
+      if (random_number == 6) {
+        which_player--;
+        noawards = true;
+      }
+      _play();
+    }
   }
 }
